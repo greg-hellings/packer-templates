b001d277c12ada3947d116f39ae2ee888f7ce4f6
Greg Hellings <greg.hellings@gmail.com>
2020-02-22 22:43:31 -0600

    Extend build timeout
    Increase all build timeouts to four hours

--- utils/build.sh
+++ utils/build.sh
@@ -24,4 +24,4 @@ timestamp="$(date +%Y%m%d)"
 # Perform actual build
 sudo -E PACKER_LOG=1 "${packer}" build -only=qemu -parallel=false \
 	-var 'headless=true' -var "version=${timestamp}" -var "disk_size=10000" \
-	"${target}"
+	-var "ssh_timeout=240m" "${target}"
