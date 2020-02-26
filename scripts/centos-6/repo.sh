8a8a4277751e82a707552638a730b23fb9155b60
Gregory Hellings <greg.hellings@gmail.com>
2020-02-26 00:45:56 -0600

    Install the actual package, ya dopes

--- scripts/centos-6/repo.sh
+++ scripts/centos-6/repo.sh
@@ -3,5 +3,5 @@
 set -e
 set -x
 
-sudo yum -y install https://download.fedoraproject.org/pub/epel/6/x86_64/epel-release-6-8.noarch.rpm
+sudo yum -y install epel-release
 sudo sed -i -e 's/^enabled=1/enabled=0/' /etc/yum.repos.d/epel.repo
