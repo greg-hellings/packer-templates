884e207cc855017088854963e828575b1ca42e95
Gregory Hellings <greg.hellings@gmail.com>
2020-09-18 23:31:50 -0500

    Fix minimize script
    You cannot write to / on Silverblue
    Silverblue apparently can't locate its swap partition, either

--- scripts/minimize.sh
+++ scripts/minimize.sh
@@ -3,8 +3,10 @@
 set -e
 set -x
 
-sudo dd if=/dev/zero of=/var/EMPTY bs=1M || :
-sudo rm /var/EMPTY
+if [[ "${PACKER_BUILD_NAME}" != *"silverblue"* ]]; then
+	dd if=/dev/zero of=/EMPTY bs=1M || :
+	rm -f /EMPTY
+fi
 
 # In CentOS 7, blkid returns duplicate devices
 swap_device_uuid=`sudo /sbin/blkid -t TYPE=swap -o value -s UUID | uniq`
@@ -15,8 +17,8 @@ elif [ -n "$swap_device_label" ]; then
   swap_device=`readlink -f /dev/disk/by-label/"$swap_device_label"`
 fi
 
-if [ ! -z "${swap_device}" ]; then
+if [ ! -z "${swap_device}" -a -e "${swap_device}" ]; then
 	sudo /sbin/swapoff "$swap_device"
-	sudo dd if=/dev/zero of="$swap_device" bs=1M || :
+	sudo dd if=/dev/zero of="$swap_device" bs=1M
 	sudo /sbin/mkswap ${swap_device_label:+-L "$swap_device_label"} ${swap_device_uuid:+-U "$swap_device_uuid"} "$swap_device"
 fi
